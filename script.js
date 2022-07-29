f8c743f6a485955bf827d007e2d2257731f9eecf
Siddhanta Chandra <iamsiddhanta.10@gmail.com>
2022-07-29 20:12:47 +0530

    added some visual queues to denote active player

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -16,7 +18,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Adding Rolling Dice Functionality
 btnRoll.addEventListener('click', function () {
@@ -30,9 +34,22 @@ btnRoll.addEventListener('click', function () {
   //Check roll number
   if (diceRoll !== 1) {
     //Add dice to current score
+
     currentScore += diceRoll;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    if (activePlayer === 0) {
+      activePlayer = 1;
+      player1El.classList.add('player--active');
+      player0El.classList.remove('player--active');
+    } else {
+      activePlayer = 0;
+      player0El.classList.add('player--active');
+      player1El.classList.remove('player--active');
+    }
   }
 });
